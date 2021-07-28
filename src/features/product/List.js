17b3f0426594ed2f5b130344785acc8c294be9d7
LucieSaulnier <lucie.saulnier1988@gmail.com>
2021-07-28 14:21:28 +0200

    feature pagination

--- src/features/product/List.js
+++ src/features/product/List.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Pagination from "@material-ui/lab/Pagination";
 
 import { Product } from "./Product";
 
+const productPerPage = 15;
 export const List = () => {
   const [products, setProducts] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (event, value) => {
+    setPage(value);
+  };
 
   useEffect(() => {
-    populateProducts();
-  }, []);
+    populateProducts(page);
+  }, [page]);
+
+  const countPages = (totalCount) => Math.ceil(totalCount / productPerPage);
 
   // une fonction qui va aller chercher la data sur l'api
   // fonction qui récupère data = fonction GET
@@ -20,13 +31,18 @@ export const List = () => {
     const result = await fetch(
       `http://localhost:6060/products?_page=${page}&_limit=${limit}`
     );
-    const data = await result.json();
-    return data;
+
+    const totalCount = result.headers.get("X-Total-Count");
+
+    setPageCount(countPages(totalCount));
+
+    const productList = await result.json();
+    return productList;
   };
 
-  const populateProducts = async () => {
+  const populateProducts = async (page) => {
     //appeler getProduct
-    const data = await getProducts(1, 15);
+    const data = await getProducts(page, productPerPage);
     // mettre le résultat dans la variable products
     setProducts(data);
   };
@@ -43,6 +59,12 @@ export const List = () => {
             />
           </Grid>
         ))}
+      </Grid>
+      <Grid>
+        <Typography>
+          Page: {page} / {pageCount}
+        </Typography>
+        <Pagination count={pageCount} page={page} onChange={handleChangePage} />
       </Grid>
     </Container>
   );
