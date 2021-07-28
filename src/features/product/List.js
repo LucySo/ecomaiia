2cd20c5a61f7ce3ca3da6ba93236cb88e9bb9d1c
LucieSaulnier <lucie.saulnier1988@gmail.com>
2021-07-28 13:00:03 +0200

    correction on display

--- src/features/product/List.js
+++ src/features/product/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
 import { Product } from "./Product";
 
@@ -16,29 +17,33 @@ export const List = () => {
   const getProducts = async (page, limit) => {
     //notation async / await pour les promises
     // aller chercher la data sur l'api
-    const result = await fetch(`http://localhost:6060/products?_page=${page}&_limit=${limit}`);
+    const result = await fetch(
+      `http://localhost:6060/products?_page=${page}&_limit=${limit}`
+    );
     const data = await result.json();
     return data;
   };
 
   const populateProducts = async () => {
     //appeler getProduct
-    const data = await getProducts();
+    const data = await getProducts(1, 15);
     // mettre le résultat dans la variable products
     setProducts(data);
   };
 
   return (
-    <Grid container spacing={3}>
-      {products.map((product) => (
-        <Grid item xs={4}>
-          <Product
-            title={product.title}
-            image={product.thumbnailUrl}
-            key={product.id}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid item xs={12} sm={6} lg={4}>
+            <Product
+              title={product.title}
+              image={product.url}
+              key={product.id}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
